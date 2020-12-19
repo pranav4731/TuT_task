401ad492f6240db909a5f5c8e8bbfae30204e08f
pranav4731 <pranavdeshpande136@gmail.com>
2020-12-18 18:18:03 -0600

    react proj v1.0

---
+++ src/index.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+//CSS
+import './index.css'
+
+//setup vars
+const author = 'Brandon Sanderson';
+const title = 'Rhythm of War';
+const img = "https://m.media-amazon.com/images/I/61ukjVCWTfL._AC_UY545_FMwebp_QL65_.jpg"
+
+function Booklist(){
+    return( 
+        <section className='booklist'>
+        <Book job='developer'/>
+        <Book title='random title' number={22}/>      
+        </section>
+    );
+}
+
+const Book = (props) => {
+    
+    return( 
+    <article className = 'book'>
+     <img src = {img} alt='' />
+     <h1>{title}</h1>
+     <h4>{author}</h4>
+     <p>{props.job}</p>
+     <p>{props.title}</p>
+     <p>{props.number}</p>
+
+    </article>    
+    );
+};
+
+
+
+
+ReactDom.render(<Booklist />, document.getElementById('root')
+);
