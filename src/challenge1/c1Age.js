bd456433c2c711448c195c5bc7ca64c94fd0b2bb
Pranav Deshpande <pranavdeshpande136@gmail.com>
2021-01-04 16:46:50 -0600

    updated

--- src/challenge1/c1Age.js
+++ src/challenge1/c1Age.js
@@ -17,3 +17,56 @@ function reset(){
 document.getElementById('ageInDays').remove();
 
 }
+
+//Challenge 5: BlackJack
+
+let blackjackGame = {
+  'you': {'scorespan' : '#your-blackjack-result', 'div' : '#your-box', 'score' : 0},  
+  'dealer': {'scorespan' : '#dealer-blackjack-result', 'div' : '#dealer-box', 'score' : 0} ,
+  'cards': ['2','3','4','5','6','7','8','9','10','J','Q','K','A'],
+};
+
+const YOU = blackjackGame['you']
+const DEALER = blackjackGame['dealer']
+
+const hitSound = new Audio('sounds/swish.m4a');
+
+document.querySelector('#blackjack-hit-button').addEventListener('click', blackjackHit);  
+
+document.querySelector('#blackjack-deal-button').addEventListener('click', blackjackDeal);  
+
+
+function blackjackHit(){
+  let card = randomCard();
+  console.log(card);
+  showCard(card, YOU);
+  
+}
+
+function randomCard(){
+  let randomIndex = Math.floor(Math.random() * 13);
+  return blackjackGame['cards'][randomIndex]
+}
+
+function showCard(card, activePlayer){
+   let cardImage = document.createElement('img');
+   cardImage.src = `images/${card}.png`;
+   document.querySelector(activePlayer['div']).appendChild(cardImage);
+   hitSound.play();
+}
+
+function blackjackDeal(){
+  let yourImages = document.querySelector('#your-box').querySelectorAll('img');
+  let dealerImages = document.querySelector('#dealer-box').querySelectorAll('img');
+
+
+
+  for(i=0; i<yourImages.length; i++){
+    yourImages[i].remove();
+  }
+
+   for(i=0; i<dealerImages.length; i++){
+    dealerImages[i].remove();
+  }
+}
+
